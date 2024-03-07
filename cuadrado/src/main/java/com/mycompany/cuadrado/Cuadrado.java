8f72b39bfba2960815912111a22d82f35649dad6
Melvin1perez <125324725+Melvin1perez@users.noreply.github.com>
2024-03-06 18:40:29 -0600

    Update Cuadrado.java

--- cuadrado/src/main/java/com/mycompany/cuadrado/Cuadrado.java
+++ cuadrado/src/main/java/com/mycompany/cuadrado/Cuadrado.java
@@ -15,29 +15,28 @@ public class Cuadrado {
     private double perimetro;
     private double area;
 
-    // Constructores
-    // Constructor vacío
+    
     public Cuadrado() {
         this.lado = 0;
         calcularPerimetro();
         calcularArea();
     }
 
-    // Constructor que recibe un lado
+    
     public Cuadrado(double lado) {
         this.lado = lado;
         calcularPerimetro();
         calcularArea();
     }
 
-    // Constructor que recibe dos lados
+    
     public Cuadrado(double lado1, double lado2) {
         this.lado = lado1;
         calcularPerimetro();
         calcularArea();
     }
 
-    // Métodos para cálculos
+    
     private void calcularPerimetro() {
         this.perimetro = 4 * this.lado;
     }
@@ -46,7 +45,7 @@ public class Cuadrado {
         this.area = this.lado * this.lado;
     }
 
-    // Método para mostrar resultados
+    
     public void mostrarResultados() {
         System.out.println("Lado: " + this.lado);
         System.out.println("Perímetro: " + this.perimetro);
@@ -56,19 +55,19 @@ public class Cuadrado {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Constructor vacío
+       
         Cuadrado cuadrado1 = new Cuadrado();
         System.out.println("constructor vacío:");
         cuadrado1.mostrarResultados();
 
-        // Constructor que recibe un lado
+        
         System.out.print("\ndigite el valor del lado del cuadrado: ");
         double lado = scanner.nextDouble();
         Cuadrado cuadrado2 = new Cuadrado(lado);
         System.out.println("\ndogite del cuadrado creado con un lado:");
         cuadrado2.mostrarResultados();
 
-        // Constructor que recibe dos lados
+        
         System.out.print("\ndigite el valor del primer lado del cuadrado: ");
         double lado1 = scanner.nextDouble();
         System.out.print("digite el valor del segundo lado del cuadrado: ");
